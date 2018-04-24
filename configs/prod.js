dc760109f2f61619c7b427309e5d48079644bb99
Matshwenyego <Ashleyngoetjana@gmail.com>
2018-04-24 11:35:53 +0200

    working tree

--- configs/prod.js
+++ configs/prod.js
@@ -1,6 +1,6 @@
 
 module.exports={
-    "database":'mongodb://app:Pa$$w0rd@ds155299.mlab.com:55299/itthynk-timesheet',
+    "database":'mongodb://app:Passw0rd@ds155299.mlab.com:55299/itthynk-timesheet',
     "port":process.env.PORT || 3000,
     "secret":'Pa$$w0rd',
     'cookieKey':'jhbsakjyfio463asydfhbfbiaw43oiufsbubfhhayyrbrr423246fsjd62hbjkjfs'
