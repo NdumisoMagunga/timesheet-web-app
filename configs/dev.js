294a2fe5555f6eac2a6ccbd8a5759fd7975b3226
Matshwenyego <Ashleyngoetjana@gmail.com>
2018-04-24 11:12:14 +0200

    t commit3

--- configs/dev.js
+++ configs/dev.js
@@ -1,6 +1,6 @@
 
 module.exports={
-    "database":'mongodb://app:P@$$w0rd@ds155299.mlab.com:55299/itthynk-timesheet',
+    "database":'mongodb://app:Pa$$w0rd@ds155299.mlab.com:55299/itthynk-timesheet',
     "port":process.env.PORT || 2000,
     "secret":'Pa$$w0rd',
     'cookieKey':'jhbsakjyfio463asydfhbfbiaw43oiufsbubfhhayyrbrr423246fsjd62hbjkjfs'
