6794f255b2930d1921d968cdc7a8be76fc1e4889
Lucky Masharane <lucky.m@itthynk.co.za>
2018-05-08 09:20:39 +0200

    admin central

--- client/src/screens/AdminCentral.js
+++ client/src/screens/AdminCentral.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
-import {Jumbotron, Row,Col, Container} from 'reactstrap';
+import {Jumbotron, Row,Col,Button, Container,TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, CardText,Form, FormGroup,Input} from 'reactstrap';
 import * as actions from '../actions'
 import {connect} from 'react-redux';
+import classnames from 'classnames';
+
 
 class AdminCentral extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          activeTab: '1'
+        };
+      }
+    
+      toggle(tab) {
+        if (this.state.activeTab !== tab) {
+          this.setState({
+            activeTab: tab
+          });
+        }
+      }
 
 render(){
     return (
@@ -20,6 +38,84 @@ render(){
                     </div>
                 </div>
             </Jumbotron>
+
+            <Container>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                        className={classnames({ active: this.state.activeTab === '1' })}
+                        onClick={() => { this.toggle('1'); }}
+                        >
+                        Venue
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                        className={classnames({ active: this.state.activeTab === '2' })}
+                        onClick={() => { this.toggle('2'); }}
+                        >
+                        Users
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                        className={classnames({ active: this.state.activeTab === '3' })}
+                        onClick={() => { this.toggle('3'); }}
+                        >
+                        Timesheets
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                        className={classnames({ active: this.state.activeTab === '4' })}
+                        onClick={() => { this.toggle('4'); }}
+                        >
+                        Submitted for Review
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        <Row>
+                        <Col sm="12">
+                            <h4>Tab 1 Contents</h4>
+                        </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <Row>
+                        <Col sm="6">
+                            <Card body>
+                            <CardTitle>Special Title Treatment</CardTitle>
+                            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                            <Button>Go somewhere</Button>
+                            </Card>
+                        </Col>
+                        <Col sm="6">
+                            <Card body>
+                            <CardTitle>Special Title Treatment</CardTitle>
+                            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                            <Button>Go somewhere</Button>
+                            </Card>
+                        </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="3">
+                        <Row>
+                        <Col sm="12">
+                            <h4>Tab 3 Contents</h4>
+                        </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="4">
+                        <Row>
+                        <Col sm="12">
+                            <h4>Tab 4 Contents</h4>
+                        </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
+            </Container>
         </div>
     )
 }
@@ -27,7 +123,8 @@ render(){
 }
 function mapStateToProps({auth, mysheets}){
     return {
-        auth
+        auth,
+        mysheets
         
     }
 }
