d1c5915cdc0d29588db3ffb12e57a404e20b4ba0
Matshwenyego_II <Ashleyngoetjana@gmail.com>
2018-05-11 12:04:11 +0200

    update

--- client/src/screens/AdminCentral.js
+++ client/src/screens/AdminCentral.js
@@ -158,13 +158,19 @@ render(){
            }  
            
            showInkBar={true}/>
-           
+         
+           <Row>
+            <Col >
           <AddVenue />
-           
-           <AssignVenue />
-            
-            </Container>
+          </Col>
 
+           <Col >
+           <AssignVenue />
+           </Col>
+           </Row>
+           </Container>
+           
+            
             
             
         </div>
