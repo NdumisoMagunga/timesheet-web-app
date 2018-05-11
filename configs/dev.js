847243f169982afe7bd558bbdb34409859fa0b7d
Matshwenyego <Ashleyngoetjana@gmail.com>
2018-05-11 10:05:24 +0200

    Friday

--- configs/dev.js
+++ configs/dev.js
@@ -1,7 +1,7 @@
 
 module.exports={
-    "database":'mongodb://localhost:27017/itthynk-timesheet',
-    //"database":'mongodb://app:Passw0rd@ds155299.mlab.com:55299/itthynk-timesheet',
+    // "database":'mongodb://localhost:27017/itthynk-timesheet',
+    "database":'mongodb://app:Passw0rd@ds155299.mlab.com:55299/itthynk-timesheet',
     "port":process.env.PORT || 2000,
     "secret":'Pa$$w0rd',
     'cookieKey':'jhbsakjyfio463asydfhbfbiaw43oiufsbubfhhayyrbrr423246fsjd62hbjkjfs'
